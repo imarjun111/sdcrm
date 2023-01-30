fcb14fcf096d136b35c78308c6ec95c0584e3962
Arjun Soni <imarjun11@gmail.com>
2023-01-30 23:39:59 +0530

    add auth service in app

--- app/Services/Auth/resources/views/welcome.blade.php
+++ app/Services/Auth/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel • Lucid12</div>
+                <div class="title">Laravel • Lucid</div>
             </div>
         </div>
     </body>
